3f2345aba00f147dd542a64c66b739ef45cab2e5
nejlasahin <2017123027@cumhuriyet.edu.tr>
2021-08-29 22:51:53 +0300

    Update RestClientController.java

--- third-homework2/src/main/java/dev/patika/thirdhomework2/api/RestClientController.java
+++ third-homework2/src/main/java/dev/patika/thirdhomework2/api/RestClientController.java
@@ -1,6 +1,7 @@
 package dev.patika.thirdhomework2.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,17 +18,22 @@ public class RestClientController {
     @Autowired
     private RestTemplate restTemplate;
 
+
     @GetMapping("/usdToTry/{amount}")
-    public double usdToTry(@PathVariable int amount){
+    public ResponseEntity<Double> usdToTry(@PathVariable int amount){
         ResponseEntity<String> result= restTemplate.getForEntity(webUrl, String.class);
-        double dollar = Double.parseDouble(result.getBody());
-        return amount;
+        String dollar = result.getBody();
+        double d=Double.parseDouble(dollar)*amount;
+        return new ResponseEntity<>(d, HttpStatus.OK);
     }
 
 
     @GetMapping("/tryToUsd/{amount}")
-    public double tryToUsd(@PathVariable int amount){
-        return amount;
+    public ResponseEntity<Double> tryToUsd(@PathVariable int amount){
+        ResponseEntity<String> result= restTemplate.getForEntity(webUrl, String.class);
+        String dollar = result.getBody();
+        double d=amount/Double.parseDouble(dollar);
+        return new ResponseEntity<>(d, HttpStatus.OK);
     }
 
 
